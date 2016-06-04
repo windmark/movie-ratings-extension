7d123e1f43b4171cd7bc0eca0adbd8ea5a7dde05
Marcus Windmark <marcus.windmark@gmail.com>
2016-06-04 14:29:17 +0200

    Call of simple API call with response

--- popup.js
+++ popup.js
@@ -5,14 +5,29 @@ $(function(){
   });
 });
 
+
+function getMovieInfo(title) {
+  return $.ajax({
+    url: "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json",
+  });
+}
+
+
+function logResults(info) {
+  console.log(info);
+  var textBox = document.getElementById('text');
+  textBox.innerHTML = info.Title + " " + info.imdbRating;
+  chrome.browserAction.setBadgeText({text: info.imdbRating});
+}
+
+
 function pasteSelection() {
   chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
 
   function(tab) {
     chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"},{frameId: 0}, function(response){
-      var text = document.getElementById('text'); 
-      console.log(response.data);
-      text.innerHTML = response.data;
+      var selectedText = response.data;
+      getMovieInfo(selectedText).done(logResults);
     });
   });
 }
