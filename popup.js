eacf85ec36900507747e8c49cf4438db55044e8a
Marcus Windmark <marcus.windmark@gmail.com>
2016-06-04 13:44:20 +0200

    Initial extension skeleton

---
+++ popup.js
@@ -0,0 +1,18 @@
+$(function(){
+  $('#paste').click(function(){
+    console.log("Click");
+    pasteSelection();
+  });
+});
+
+function pasteSelection() {
+  chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
+
+  function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, function(response){
+      var text = document.getElementById('text'); 
+      console.log(response.data);
+      text.innerHTML = response.data;
+    });
+  });
+}
