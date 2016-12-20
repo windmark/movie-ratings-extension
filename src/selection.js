ad2b3765254aacaab437225b56ca8dc01b949eab
Marcus Windmark <marcus@Ensio.lan>
2016-12-20 15:55:14 +0100

    Now stripping if selection includes episode

--- src/selection.js
+++ src/selection.js
@@ -2,23 +2,31 @@ document.addEventListener('mouseup',function(event) {
     var sel = $.trim(window.getSelection().toString());
 
     var title = ""
-    var year_list = []
-    var year_regexp = /\d{4}/g;
+    var yearList = []
+    var yearRegex = /\d{4}/g;
+    var episodeRegex = /s[0-9]*e[0-9]*/gi;
     
-    while(year_regexp.test(sel) == true) {
-    	matched_index = year_regexp.lastIndex - 4
-    	year = sel.substr(matched_index, 4)
-    	year_list.push([year, matched_index])
+    while(yearRegex.test(sel) == true) {
+    	matchedIndex = yearRegex.lastIndex - 4
+    	year = sel.substr(matchedIndex, 4)
+    	yearList.push([year, matchedIndex])
     }
 
-    if(year_list.length > 0) {
-    	title = $.trim(sel.substr(0, year_list[year_list.length - 1][1]))
-    	year = year_list[year_list.length - 1][0]
+    if(yearList.length > 0) {
+    	title = $.trim(sel.substr(0, yearList[yearList.length - 1][1]))
+    	year = yearList[yearList.length - 1][0]
     } else {
     	title = sel
     	year = ""
     }
-    
+
+    if(episodeRegex.test(title)) {
+    	matchedIndex = episodeRegex.lastIndex
+    	episodeRegex.lastIndex = 0
+    	episode = episodeRegex.exec(title)[0]
+    	title = title.substring(0, matchedIndex - episode.length)
+    }
+
     message = {"title": title, "year": year}
     
     if(sel.length) {
