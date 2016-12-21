2df657dfd2eba4252c312b678406217f508a1ae9
Marcus Windmark <marcus.windmark@gmail.com>
2016-12-21 12:50:16 +0100

    Added context menu event to support immedate results when opened

--- src/selection.js
+++ src/selection.js
@@ -1,10 +1,17 @@
-document.addEventListener('mouseup',function(event) {
-    var sel = $.trim(window.getSelection().toString());
-    movieInfo = processSelection(sel)
+// Event for contextmenu added to support immediate results when context menu
+// is opened without mouseup event firing. Happens on two finger tap on touchpad
+// among others.
+eventList = ['mouseup', 'contextmenu']
 
-    if(sel.length) {
-		chrome.runtime.sendMessage({'method': 'setMovie', 'message': movieInfo})
-    } else {
-    	chrome.runtime.sendMessage({'method': 'resetBadge'})
-    }
+eventList.forEach(function(eventName) {
+	document.addEventListener(eventName,function(event) {
+		var sel = $.trim(window.getSelection().toString());
+		movieInfo = processSelection(sel)
+
+		if(sel.length) {
+			chrome.runtime.sendMessage({'method': 'setMovie', 'message': movieInfo})
+		} else {
+			chrome.runtime.sendMessage({'method': 'resetBadge'})
+		}
+	});
 });
